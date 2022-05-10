69e33fb6a2b2c53e1edff3f19ec5fea7721b7820
EsraaBanat <esraa.bnt96@gmail.com>
2022-05-10 20:24:55 +0300

    add tours

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,15 +2,22 @@ import React from 'react';
 import Header from '../header/Header'
 import Tours from '../tours/Tours'
 import Footer from '../footer/Footer'
+const travData = require('../../data/db.json');
 
 
 const Home = () => {
   return (
     <>
       <Header title='Travel Destination' />
-      {
-      <Tours />
+
+     {
+        travData.map(element => {
+          return (
+            <Tours image={element.image} alt={element.name} name={element.name} />
+          )
+        })
       }
+
       <Footer url='www.facebook.com' url2='www.Instagram.com' url3='www.LinkedIn.com' name='Bahaa/Esraa' />
     </>
   );
