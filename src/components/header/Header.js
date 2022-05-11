6c477aad8a1255eaeedc91408684ded570ae4c08
BahaaNimer <bahaanimer97@gmail.com>
2022-05-12 00:25:44 +0300

    first staage

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import NavBar from '../NavBar/NavBar.js';
 
 const Header = (params) => {
   return (
     <>
       <h1>{params.title}</h1>
+      <NavBar />
     </>
   );
 }
